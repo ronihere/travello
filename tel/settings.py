69ec1abcd37afaff329f85b8dea61ea634bf5bbe
ronihere <74338794+ronihere@users.noreply.github.com>
2022-01-23 16:15:43 +0530

    Update settings.py

--- tel/settings.py
+++ tel/settings.py
@@ -131,7 +131,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
-MEDIA_URL='/images/'
 
 
